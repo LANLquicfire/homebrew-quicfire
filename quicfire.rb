d1ebda52c6d3dffe0d9a9d46dba492b99117bafa
LANLquicfire <66660841+LANLquicfire@users.noreply.github.com>
2020-06-09 00:21:03 -0600

    Update quicfire.rb

--- quicfire.rb
+++ quicfire.rb
@@ -1,10 +1,11 @@
-class QuicFire < Formula
+class Quicfire < Formula
   desc "Installs quicfire via homebrew and manages all dependencies"
-  
+  homepage "https://github.com/LANLquicfire/quicfire"
+  url "https://github.com/LANLquicfire/quicfire/blob/master/QUIC_WildFire.zip?raw=true", :using => :curl
+
   depends_on "python"
   depends_on "curl"
   
-  url "https://github.com/LANLquicfire/quicfire/blob/master/QUIC_WildFire.zip?raw=true", :using => :curl
   
   resource "numpy" do
     url "https://files.pythonhosted.org/packages/84/1e/ff467ac56bfeaea51d4a2e72d315c1fe440b20192fea7e460f0f248acac8/numpy-1.18.2.zip"
@@ -20,5 +21,12 @@ class QuicFire < Formula
     url "https://files.pythonhosted.org/packages/4a/30/eb8e7dd8e3609f05c6920fa82f189302c832e5a0f6667aa96f952056bc0c/matplotlib-3.2.1.tar.gz"
     sha256 "ffe2f9cdcea1086fc414e82f42271ecf1976700b8edd16ca9d376189c6d93aee"
   end
+  
+  def install
+  end
+  
+  test do
+  end
+  
 end
   
